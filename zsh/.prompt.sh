92ce1a46d9ec275067f1cde484ef77460b8adcd0
David Noland <david.noland@wpengine.com>
2022-12-16 10:41:43 -0600

    Update .prompt.sh

--- zsh/.prompt.sh
+++ zsh/.prompt.sh
@@ -6,9 +6,9 @@ parse_git_branch() {
 #Execute function assign branch name to a variable
 git_check=$(parse_git_branch)
 
-#Conditional check on repo branch and define prompt accordingly
+#Conditional check on repo directory (3 folders back) and branch and define prompt accordingly
 if [[ -z $git_check ]]; then
-	export PS1="%F{green}%W%t %n - %1~%F{white} > "  # If not in an active git repo
+	export PS1="%F{green}%W%t %n - %3~%F{white} > "  # If not in an active git repo
 else
-	export PS1="%F{green}%W%t %n - %1~ %F{white}(%F{cyan}$git_check%F{white}) > "  # If in an active git repo
+	export PS1="%F{green}%W%t %n - %3~ %F{white}(%F{cyan}$git_check%F{white}) > "  # If in an active git repo
 fi
